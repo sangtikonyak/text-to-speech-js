1ed14f97f54be0d627140206e7e2236d5a52e8fa
sangti <ssangtikonyak@gmail.com>
2023-11-25 21:36:15 +0530

    final commit

---
+++ text to speech js/script.js
@@ -0,0 +1,24 @@
+let speech = new SpeechSynthesisUtterance();
+
+let addVoices = [];
+
+let voiceSelect = document.querySelector('select');
+window.speechSynthesis.onvoiceschanged=()=>{
+    addVoices = window.speechSynthesis.getVoices();
+    speech.voice = addVoices[0];
+
+    addVoices.forEach((voice,i)=>{
+        voiceSelect.options[i] = new Option(voice.name, i)
+    })
+};
+
+voiceSelect.addEventListener('change',()=>{
+    console.log(voiceSelect.value)
+    speech.voice = addVoices[voiceSelect.value]
+})
+
+
+document.querySelector('button').addEventListener('click',()=>{
+    speech.text = document.querySelector('textarea').value;
+    window.speechSynthesis.speak(speech)
+});
